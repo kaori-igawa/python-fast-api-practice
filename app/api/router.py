03765559dd85bfa07e534d766ab56c5428a31602
kaori-igawa <kaori0v0igawa@gmail.com>
2025-12-07 18:18:03 +0900

    [A-1] [A-2] 複雑なスキーマの検討

--- app/api/router.py
+++ app/api/router.py
@@ -9,6 +9,7 @@ from .v1.fastapi_async.router import router as fastapi_async
 from .v1.fastapi_router_refactoring.router import api_router  as fastapi_router_refactoring
 from .v1.fastapi_di.router import router  as fastapi_di
 from .v1.fastapi_memoapp.router import router  as fastapi_memoapp
+from .v1.appendix.router import router as appendix
 
 api_router = APIRouter()
 api_router.include_router(hello.router)
@@ -21,3 +22,4 @@ api_router.include_router(fastapi_async)
 api_router.include_router(fastapi_router_refactoring)
 api_router.include_router(fastapi_di)
 api_router.include_router(fastapi_memoapp)
+api_router.include_router(appendix)
